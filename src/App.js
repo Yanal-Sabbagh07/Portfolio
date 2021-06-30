56bcb07941ec6a8f907ed93cd78316a64d484c90
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2021-06-30 13:49:55 +0200

    create contact section and sending emails

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
+
 function App() {
   const [menuOpen,setMenuOpen] = useState(false);
-  
   return (
     <div className="app">
       <Topbar 
