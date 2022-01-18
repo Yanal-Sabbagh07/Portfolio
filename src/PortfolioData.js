19561e723131937117fdd31cbc36a5655d83c11b
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-01-18 09:22:37 +0100

    adding fittery to the projects

--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -1,36 +1,37 @@
- const PortfolioData = [
-    {
-        id:1,
-        name:"Omnifood",
-        description:"omnifood is a pixel perfect web app build with pure html and css.",
-        img:"assets/OmniFood.png",
-        demoLink:"https://yanal-sabbagh07.github.io/Omnifood/",
-        githubLink:"https://github.com/Yanal-Sabbagh07/Omnifood"
-    },
-    {
-        id:2,
-        name:"Natours",
-        description:"Natours is a pixel perfect web app build with pure html and sass.",
-        img:"assets/natours.png",
-        demoLink:"https://yanal-sabbagh07.github.io/Natours/",
-        githubLink:"https://github.com/Yanal-Sabbagh07/Natours"
-    },
-    {
-        id:3,
-        name:"Budgety",
-        description:"Budgety is a  web app build with vanilla javascript",
-        img:"assets/budgety.png",
-        demoLink:"https://yanal-sabbagh07.github.io/Budgety/",
-        githubLink:"https://github.com/Yanal-Sabbagh07/Budgety"
-    },
-    {
-        id:4,
-        name:"Keeper",
-        description:"Keeper is a cloe of google keep  build with React.js",
-        img:"assets/keeper.png",
-        demoLink:"https://yanal-sabbagh07.github.io/keeper/",
-        githubLink:"https://github.com/Yanal-Sabbagh07/keeper"
-    },
-    
+const PortfolioData = [
+  {
+    id: 1,
+    name: "Omnifood",
+    description:
+      "omnifood is a pixel perfect web app build with pure html and css.",
+    img: "assets/OmniFood.png",
+    demoLink: "https://yanal-sabbagh07.github.io/Omnifood/",
+    githubLink: "https://github.com/Yanal-Sabbagh07/Omnifood",
+  },
+  {
+    id: 2,
+    name: "Natours",
+    description:
+      "Natours is a pixel perfect web app build with pure html and sass.",
+    img: "assets/natours.png",
+    demoLink: "https://yanal-sabbagh07.github.io/Natours/",
+    githubLink: "https://github.com/Yanal-Sabbagh07/Natours",
+  },
+  {
+    id: 3,
+    name: "Budgety",
+    description: "Budgety is a  web app build with vanilla javascript",
+    img: "assets/budgety.png",
+    demoLink: "https://yanal-sabbagh07.github.io/Budgety/",
+    githubLink: "https://github.com/Yanal-Sabbagh07/Budgety",
+  },
+  {
+    id: 4,
+    name: "fittery",
+    description: "Keeper is a cloe of google keep  build with React.js",
+    img: "assets/fittery.png",
+    demoLink: "https://yanal-sabbagh07.github.io/fittery/",
+    githubLink: "https://github.com/Yanal-Sabbagh07/fittery",
+  },
 ];
-export  default PortfolioData;
+export default PortfolioData;
