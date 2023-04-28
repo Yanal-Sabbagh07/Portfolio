503b361ebbeac01d7770cfbb291cff3d53919f31
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-04-28 18:10:24 +0200

    edit the height of all sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./app.scss";
-import {useState} from "react";
+import { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
 import Menu from "./components/menu/Menu";
 import Intro from "./components/intro/Intro";
@@ -8,22 +8,16 @@ import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
 
 function App() {
-  const [menuOpen,setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar 
-        menu={menuOpen}
-        setMenu={setMenuOpen}
-      />
-      <Menu 
-      	menu={menuOpen}
-        setMenu={setMenuOpen}
-      />
+      <Topbar menu={menuOpen} setMenu={setMenuOpen} />
+      <Menu menu={menuOpen} setMenu={setMenuOpen} />
       <div className="sections">
-      <Intro />
-      <Portfolio/>
-      <Skills />
-      <Contact />
+        <Intro />
+        <Portfolio />
+        <Skills />
+        <Contact />
       </div>
     </div>
   );
