5780b6d1fa99e21953dd2263b27bdd820e376c0f
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-04-29 10:20:58 +0200

    portfolio slider functionality auto and touch

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,65 @@
 import "./Portfolio.scss";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import PortfolioData from "../../PortfolioData";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Portfolio = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [delay, setDelay] = useState(10000);
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const minSwipeDistance = 50;
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe) {
+      setCurrentSlide(currentSlide < 5 ? currentSlide + 1 : 5);
+      setDelay(500000);
+    } else if (isRightSwipe) {
+      setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 0);
+      setDelay(500000);
+    }
+  };
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+      let id = setInterval(() => {
+        savedCallback.current();
+      }, delay);
+      return () => clearInterval(id);
+    }, [delay]);
+  }
 
   const handleClick = (direction) => {
+    setDelay(500000);
     direction === "left"
       ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3)
       : setCurrentSlide(currentSlide < 3 ? currentSlide + 1 : 0);
   };
+  useInterval(() => {
+    if (currentSlide < 5) {
+      setCurrentSlide(currentSlide + 1);
+    } else {
+      setCurrentSlide(0);
+    }
+  }, delay);
   return (
     <div className="portfolio" id="portfolio">
       <div className="portfolioHeader">
@@ -20,6 +68,9 @@ const Portfolio = () => {
 
       <div
         className="slider"
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
         style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
       >
         {PortfolioData.map((project) => (
